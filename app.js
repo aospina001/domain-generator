4f440ffef777be61d0a132aa4d9eab064933d1e1
aospina001 <aospina001@gmail.com>
2020-10-02 22:21:55 +0000

    Added more strings to the arrays

---
+++ app.js
@@ -0,0 +1,10 @@
+function domainGenerator () {
+    let pronoun = ['the','our','my','their','your',];
+    let adj = ['great', 'big', 'funny','boring','beautiful','comical','smart','athletic'];
+    let noun = ['jogger','racoon','dog','wife','child','coder','monkey'];
+    let domain = ['.com','.net','.org','.io']
+
+    
+};
+
+domainGenerator();
