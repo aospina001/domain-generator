bc8113f6fb8696de91ad970a48b5260a65c43214
aospina001 <aospina001@gmail.com>
2020-10-05 13:51:02 +0000

    Finalized domain generator

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ function domainGenerator () {
     let pronoun = ['the','our','my','their','your',];
     let adj = ['great', 'big', 'funny','boring','beautiful','comical','smart','athletic'];
     let noun = ['jogger','racoon','dog','wife','child','coder','monkey'];
-    let domain = ['.com','.net','.org','.io']
+    let domain = ['.com','.net','.org','.io'];
 
+    console.log(pronoun[Math.floor(Math.random() * pronoun.length)] + adj[Math.floor(Math.random() * adj.length)] + noun[Math.floor(Math.random() * pronoun.length)] + domain[Math.floor(Math.random() * domain.length)]);
     
 };
 
